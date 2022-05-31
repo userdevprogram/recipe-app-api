f3c93f16138c9dd4dff7338719260a27ae887350
Dmytro <atlantida.goods@gmail.com>
2022-05-31 08:19:04 +0300

    test command

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgreaql',
-        'HOST': os.environ('DB_HOST'),
-        'NAME': os.environ('DB_NAME'),
-        'USER': os.environ('DB_USER'),
-        'PASSWORD': os.environ('DB_PASS'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
